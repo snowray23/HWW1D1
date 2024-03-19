e003f0c1f74e48b1bd8e81ee9e6a5d2b1c9bd4ec
Raymond Zheng <raymondzheng1994@gmail.com>
2024-03-19 08:41:07 -0400

    first commit

---
+++ day1hw.py
@@ -0,0 +1,52 @@
+#Task1
+weather = "sunny"
+
+if weather == "sunny":
+    print("Wear sunglasses!")
+else:
+    print("Take an umbrella!")
+
+feeling = "happy"
+if feeling == "happy":
+    print("Thats great to hear!")
+else:
+    print("I hope your day gets better!")
+
+#task 2
+PI_VALUE = 3.14
+userAge = 25
+user_location = "New York"
+max_limt = 1000
+
+#task 3
+variable_a = "Hello, World!"
+print(type(variable_a))
+
+variable_b = 23
+print(type(variable_b))
+
+variable_c = 3.14
+print(type(variable_c))
+
+variable_d = True
+print(type(variable_d))
+
+#task 4
+apple_cost = 10
+water_cost = 15
+bread_cost = 12
+food_basket = apple_cost + water_cost + bread_cost
+print(food_basket)
+
+money_in_savings = 15000
+interest_rate = 10
+total_savings = money_in_savings + (money_in_savings * interest_rate /100)
+print(total_savings)
+
+#task 5
+num1 = 12
+num2 = 10
+num1 = 10
+num2 = 12
+if num2 == 12:
+    print("yay!")
